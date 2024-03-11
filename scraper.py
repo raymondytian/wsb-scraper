4b1acd4a2839eeebeb54bbd56365e8b7ccbbf3ad
raymondytian <rymdtian@gmail.com>
2024-03-11 02:00:40 -0500

    init

---
+++ scraper.py
@@ -0,0 +1,23 @@
+import praw
+import re
+import os
+import numpy as np
+from collections import Counter
+from pathlib import Path
+from datetime import datetime
+
+
+# GLOBALS
+base_path = Path(__file__).parent
+
+
+def redditInit():
+    
+
+
+def main():
+    reddit = 
+
+
+if __name__ == "__main__":
+    main()
